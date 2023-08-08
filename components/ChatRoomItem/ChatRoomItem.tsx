dd1272ec8b8ae3a793070f8eefba34de1b5d846b
Dibyendu Dey <dibyendudey303@gmail.com>
2023-08-08 12:23:44 +0530

    Fetched chat rooms

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -1,31 +1,59 @@
-import React from "react";
-import { Text, View, Image, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, Image, Pressable, ActivityIndicator } from "react-native";
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { ChatRoomUser, User } from "../../src/models";
+import { Auth, DataStore } from "aws-amplify";
 
 const ChatRoomItem = ({ chatRoom }) => {
-  const user = chatRoom.users[1];
+  // const [users, setUsers] = useState<User[]>([]);
+  const [displayUser, setDisplayUser] = useState<User | null>(null);
+
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const chatRoomUserIds = (await DataStore.query(ChatRoomUser))
+        .filter((chatRoomUser) => chatRoomUser.chatRoomId === chatRoom.id)
+        .map((user) => user.userId);
+
+      const chatRoomUsers = await Promise.all(
+        chatRoomUserIds.map(async (id) => await DataStore.query(User, id))
+      );
+      console.log(chatRoomUsers);
+      // setUsers(chatRoomUsers);
+      const authUser = await Auth.currentAuthenticatedUser();
+      setDisplayUser(
+        chatRoomUsers.find((user) => user.id !== authUser.attributes.sub) ||
+          null
+      );
+    };
+    fetchUsers();
+  }, []);
 
   const onPress = () => {
     navigation.navigate("ChatRoom", { id: chatRoom.id });
   };
 
+  if (!displayUser) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <Pressable onPress={onPress} style={styles.container}>
-      <Image source={{ uri: user.imageUri }} style={styles.image} />
-      {chatRoom.newMessages && (
+      <Image source={{ uri: displayUser.imageUri }} style={styles.image} />
+      {!!chatRoom.newMessages && (
         <View style={styles.badgeContainer}>
           <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
         </View>
       )}
       <View style={styles.rightContainer}>
         <View style={styles.row}>
-          <Text style={styles.name}>{user.name}</Text>
-          <Text style={styles.text}>{chatRoom.lastMessage.createdAt}</Text>
+          <Text style={styles.name}>{displayUser.name}</Text>
+          <Text style={styles.text}>{chatRoom.lastMessage?.createdAt}</Text>
         </View>
         <Text numberOfLines={1} style={styles.text}>
-          {chatRoom.lastMessage.content}
+          {chatRoom.lastMessage?.content}
         </Text>
       </View>
     </Pressable>
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, FlatList, Pressable, Text } from "react-native";
 import ChatRoomItem from "../components/ChatRoomItem";
 
 import ChatRoomsData from "../assets/dummy-data/ChatRooms";
-import { Auth } from "aws-amplify";
+import { Auth, DataStore } from "aws-amplify";
+import { ChatRoom, ChatRoomUser } from "../src/models";
 
 export default function HomeScreen() {
+  const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+
+  useEffect(() => {
+    const fetchChatRooms = async () => {
+      const currUser = await Auth.currentAuthenticatedUser();
+      const currUserId = currUser.attributes.sub;
+      console.log("My ID: ", currUserId);
+
+      console.log("Fetching chat room users");
+      const chatRoomUsers = await DataStore.query(ChatRoomUser);
+      console.log("Successfully fetched chat room users");
+
+      const myChatRooms = chatRoomUsers
+        .filter((chatRoomUser) => chatRoomUser.userId === currUserId)
+        .map((chatRoomUser) => chatRoomUser.chatRoomId);
+
+      console.log(myChatRooms);
+
+      const fetchedChatRooms = await Promise.all(
+        myChatRooms.map(async (id) => await DataStore.query(ChatRoom, id))
+      );
+      console.log(fetchedChatRooms);
+      setChatRooms(fetchedChatRooms);
+    };
+    fetchChatRooms();
+  }, []);
   const logOut = () => {
     Auth.signOut();
   };
   return (
     <View style={styles.page}>
       <FlatList
-        data={ChatRoomsData}
+        data={chatRooms}
         renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
       />
       <Pressable
